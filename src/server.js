b630366b975e28128bbc20c2f009eedc9f0d740f
Stanislav Popov <popstas@gmail.com>
2020-12-25 23:23:28 +0500

    refactor(server): clean file

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 const program = require("./program");
 const scrapSite = require("./scrap-site");
 
+const queue = require("queue");
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const http = require("http").createServer(app);
+
 const io = require("socket.io")(http, {
   cors: {
     origin: "*",
@@ -13,17 +15,69 @@ const io = require("socket.io")(http, {
   },
 });
 
-const queue = require("queue");
-const maxConcurrency = 2;
-const q = queue({
-  concurrency: maxConcurrency,
-  autostart: true,
-});
-let scansTotal = 0;
 
-q.on("success", function (result, job) {
-  scansTotal++;
-});
+const maxConcurrency = 2;
+let scansTotal = 0;
+let q;
+initQueue();
+initExpress(app);
+io.on("connection", onSocketConnection);
+
+
+
+
+
+
+async function onScan(url, args, socket) {
+  // log(`> site-audit-seo ` + args, socket);
+  args = args.split(" ");
+  if (!url) {
+    log("URL not defined!", socket);
+    return;
+  }
+
+  program.exitOverride();
+  try {
+    program.parse([...["", ""], ...args]);
+  } catch (e) {
+    log("failed to parse arguments: " + e.message, socket);
+    return;
+  }
+
+  await program.postParse();
+
+  const opts = program.getOptions();
+  opts.webService = true;
+  opts.socket = socket;
+
+  // console.log('opts: ', opts);
+  program.outBrief(opts);
+
+  // try {
+  if (serverState()['running'] >= maxConcurrency) {
+    log('Pending...', socket, true);
+  }
+  q.push(async function () {
+    log(`Start audit: ${url}`, socket, true);
+    const res = await scrapSite(url, opts);
+    log(`Finish audit: ${url}`, socket, true);
+    return res;
+  });
+}
+
+function initQueue() {
+  q = queue({
+    concurrency: maxConcurrency,
+    autostart: true,
+  });
+  q.on("success", function (result, job) {
+    scansTotal++;
+  });
+  q.start((err) => {
+    if (err) throw err;
+    console.log("all done!");
+  });
+}
 
 function serverState() {
   return {
@@ -38,49 +92,7 @@ function sendStats(socket) {
   socketSend(socket, "serverState", serverState());
 }
 
-function submitQueueEvents(socket) {
-  q.on("success", function (result, job) {
-    console.log("job finished processing:", job.toString().replace(/\n/g, ""));
-    sendStats(socket);
-  });
-  q.on("timeout", function (next, job) {
-    console.log("job timed out:", job.toString().replace(/\n/g, ""));
-    next();
-  });
-  q.on("start", function (job) {
-    console.log("jobs started", job);
-    sendStats(socket);
-  });
-  q.on("error", function (err, job) {
-    console.log("error:", err);
-    console.log("job:", job);
-  });
-}
-
-q.start((err) => {
-  if (err) throw err;
-  console.log("all done!");
-});
-
-const port = process.env.PORT || 5301;
-
-function log(msg, socket, outTime=false) {
-  if (outTime) {
-    msg = new Date().toTimeString().split(' ')[0] + ' ' + msg;
-  }
-  console.log(msg);
-  socketSend(socket, "status", msg);
-}
-
-function socketSend(socket, event, msg) {
-  if (socket) {
-    // console.log('socket: ' + event, msg);
-    console.log(event + socket.uid + ": ", msg);
-    socket.emit(event + (socket.uid || ""), msg);
-  }
-}
-
-io.on("connection", (socket) => {
+function onSocketConnection(socket) {
   log("user connected to server", socket, true);
 
   submitQueueEvents(socket);
@@ -103,138 +115,73 @@ io.on("connection", (socket) => {
     sendStats(socket);
   });
 
-  socket.on("scan", async ({ url, args }) => {
-    // log(`> site-audit-seo ` + args, socket);
-    args = args.split(" ");
-    if (!url) {
-      log("URL not defined!", socket);
-      return;
-    }
-
-    program.exitOverride();
-    try {
-      program.parse([...["", ""], ...args]);
-    } catch (e) {
-      log("failed to parse arguments: " + e.message, socket);
-      return;
-    }
-
-    await program.postParse();
-
-    const opts = program.getOptions();
-    opts.webService = true;
-    opts.socket = socket;
-
-    // console.log('opts: ', opts);
-    program.outBrief(opts);
-
-    // try {
-    if (serverState()['running'] >= maxConcurrency) {
-      log('Pending...', socket, true);
-    }
-    q.push(async function () {
-      log(`Start audit: ${url}`, socket, true);
-      const res = await scan(url, opts);
-      log(`Finish audit: ${url}`, socket, true);
-      return res;
-    });
+  socket.on("scan", ({url, args}) => {
+    return onScan(url, args, socket)
   });
 
   socket.on("disconnect", () => {
     clearInterval(interval);
     console.log("user disconnected");
   });
-});
-
-http.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`);
-});
-
-// CORS
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use("/reports", express.static("data/reports"));
-
-/* app.post("/scan", async (req, res) => {
-  const url = req.body.url;
-  const args = req.body.args.split(" ");
-  if (!url) {
-    log("URL not defined!");
-    return;
-  }
-
-  program.exitOverride();
-  try {
-    program.parse([...["", ""], ...args]);
-  } catch (e) {
-    log("failed to parse arguments: " + e.message);
-    res.send("ERROR");
-    return;
-  }
-
-  await program.postParse();
-
-  const opts = program.getOptions();
-  opts.webService = true;
-  opts.socket = userSocket;
-
-  program.outBrief(opts);
-
-  // try {
-    q.push(async function() {
-      return await scan(url, opts);
-    });
-  // } catch (e) {
-    // log("error while scan:", e.message);
-  // }
-
-  res.send("OK");
-}); */
-
-app.get("/", async (req, res) => {
-  res.send("site-audit-seo working");
-});
-
-async function scan(url, opts) {
-  const data = await scrapSite(url, opts);
-  return data;
 }
 
-/* function reqQueryToArgs(req) {
-  if (req.query.url) {
-    req.query.urls = req.query.url;
-    delete req.query.url;
-  }
+function submitQueueEvents(socket) {
+  q.on("success", function (result, job) {
+    console.log("job finished processing:", job.toString().replace(/\n/g, ""));
+    sendStats(socket);
+  });
+  q.on("timeout", function (next, job) {
+    console.log("job timed out:", job.toString().replace(/\n/g, ""));
+    next();
+  });
+  q.on("start", function (job) {
+    console.log("jobs started", job);
+    sendStats(socket);
+  });
+  q.on("error", function (err, job) {
+    console.log("error:", err);
+    console.log("job:", job);
+  });
+}
 
-  const args = [process.argv[0], process.argv[1]];
-  for (let reqParam in req.query) {
-    for (let opt of program.options) {
-      let argNames = [];
-      argNames.push(opt.long.replace(/^--/, ""));
-      if (opt.short) argNames.push(opt.short.replace(/^-/, ""));
-      if (opt.negate) {
-        for (let i in argNames) {
-          argNames[i] = argNames[i].replace(/^no-/, "");
-        }
-      }
-
-      // add arg as in command line
-      if (argNames.includes(reqParam)) {
-        args.push("--" + argNames[0]);
-        args.push(req.query[reqParam]);
-      }
-    }
+function log(msg, socket, outTime=false) {
+  if (outTime) {
+    msg = new Date().toTimeString().split(' ')[0] + ' ' + msg;
   }
-  console.log("args: ", args);
-  return args;
-} */
+  console.log(msg);
+  socketSend(socket, "status", msg);
+}
+
+function socketSend(socket, event, msg) {
+  if (socket) {
+    // console.log('socket: ' + event, msg);
+    console.log(event + socket.uid + ": ", msg);
+    socket.emit(event + (socket.uid || ""), msg);
+  }
+}
+
+function initExpress(app) {
+  // CORS
+  app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
+
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.use("/reports", express.static("data/reports"));
+
+  app.get("/", async (req, res) => {
+    res.send("site-audit-seo working");
+  });
+
+  const port = process.env.PORT || 5301;
+  http.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}`);
+  });
+}
